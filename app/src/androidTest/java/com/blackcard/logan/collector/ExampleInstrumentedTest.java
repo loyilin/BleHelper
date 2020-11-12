1ea33269f7f6254ae13f3f8c876a15c87e3289df
Logan <1913656443@qq.com>
2020-11-12 16:40:56 +0800

    更新md

--- app/src/androidTest/java/com/blackcard/logan/collector/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/blackcard/logan/collector/ExampleInstrumentedTest.java
@@ -2,25 +2,26 @@ package com.blackcard.logan.collector;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.MediumTest;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@MediumTest
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("com.blackcard.logan.collector", appContext.getPackageName());
     }
 }
